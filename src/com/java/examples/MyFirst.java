f08a938d73b80a5b7473e5bf973facfc862844b0
Abdul <Abdul@DESKTOP-31J9UT2>
2020-09-29 10:36:20 +0530

    Core java Test

---
+++ src/com/java/examples/MyFirst.java
@@ -0,0 +1,10 @@
+package com.java.examples;
+
+
+public class MyFirst 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Hi");
+	}
+}
---
+++ src/com/java/examples/Test.java
@@ -0,0 +1,12 @@
+package com.java.examples;
+
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		int[] x = new int[10];
+		System.out.println(x[0]);
+		System.out.println(x.length);
+	}
+
+}
